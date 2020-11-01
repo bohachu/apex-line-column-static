a38db5ef4d7ea664df3b9ee6f5e92bf40f966a74
bohachu <cbh@cameo.tw>
2020-11-01 12:47:32 +0800

    dynamicallyLoadScript

--- js/apex-line-column.js
+++ js/apex-line-column.js
@@ -1,4 +1,12 @@
 //apex-line-column.js
+
+function dynamicallyLoadScript(url) {
+  var script = document.createElement("script"); // create a script DOM node
+  script.src = url; // set its src to the provided URL
+  document.head.appendChild(script); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+dynamicallyLoadScript("https://cdn.jsdelivr.net/npm/apexcharts");
+
 class ApexLineColumn extends HTMLElement {
   connectedCallback() {
     this.str_random_id = "id_" + Math.random().toString(36).substr(2, 9);
